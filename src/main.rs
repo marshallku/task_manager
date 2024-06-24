b8125337146f3b9d33fac58af92c900acfe13957
Marshall Ku <marshall@kakao.com>
2024-06-24 19:26:11 +0900

    Add commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Task Manager")]
+#[command(about = "A simple CLI task manager")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add,
+    List,
+    Update,
+    Delete,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Add => {
+            unimplemented!("Add task")
+        }
+        Commands::List => {
+            unimplemented!("List tasks")
+        }
+        Commands::Update => {
+            unimplemented!("Update task")
+        }
+        Commands::Delete => {
+            unimplemented!("Delete task")
+        }
+    }
 }
