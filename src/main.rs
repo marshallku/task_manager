482418df524d3020f1fe84f8c9c21082b2ca2950
Marshall Ku <marshall@kakao.com>
2024-08-08 18:00:10 +0900

    Simplify codes with better error handling

--- src/main.rs
+++ src/main.rs
@@ -30,112 +30,67 @@ fn main() {
     let args = Cli::parse();
     let mut tasks: Vec<Task> = load_tasks().unwrap_or_else(|_| Vec::new());
 
-    match args.command {
-        Commands::Add => {
-            let name = match Text::new("Enter task name: ").prompt() {
-                Ok(name) => name,
-                Err(_) => panic!("Failed to get task name"),
-            };
-            let deadline = match DateSelect::new("Enter task deadline (YYYY-MM-DD): ")
-                .with_min_date(NaiveDate::from(Utc::now().naive_utc()))
-                .prompt()
-            {
-                Ok(deadline) => deadline,
-                Err(_) => panic!("Failed to get task deadline"),
-            };
-            let priority = match Select::new("Enter task priority: ", vec!["Low", "Medium", "High"])
-                .prompt()
-            {
-                Ok(priority) => priority.to_string(),
-                Err(_) => panic!("Failed to get task priority"),
-            };
-            let estimated_hours: f32 = match CustomType::<f32>::new("Enter task estimated hours: ")
-                .with_formatter(&|i| format!("{:.2}h", i))
-                .with_error_message("Please type a valid number")
-                .prompt()
-            {
-                Ok(estimated_hours) => estimated_hours,
-                Err(_) => panic!("Failed to get task estimated hours"),
-            };
-
-            let status = TaskStatus::Todo;
-
-            match add_task(
-                &mut tasks,
-                name,
-                status,
-                deadline,
-                priority,
-                estimated_hours,
-            ) {
-                Ok(_) => {
-                    save_tasks(&tasks).expect("Failed to save tasks");
-                }
-                Err(err) => match err {
-                    TaskError::InvalidState(state) => {
-                        eprintln!("Invalid task state: {}", state);
-                    }
-                    _ => {
-                        eprintln!("Failed to add task: {}", err);
-                    }
-                },
-            }
+    let result = match args.command {
+        Commands::Add => add_task_interactive(&mut tasks),
+        Commands::List => {
+            list_tasks(&tasks);
+            Ok(())
         }
-        Commands::List => list_tasks(&tasks),
-        Commands::Start { id } => match start_task(&mut tasks, id) {
-            Ok(_) => {
-                save_tasks(&tasks).expect("Failed to save tasks");
-            }
-            Err(err) => match err {
-                TaskError::NotFound => {
-                    eprintln!("Task not found");
-                }
-                TaskError::InvalidState(state) => {
-                    eprintln!("Invalid task state: {}", state);
-                }
-            },
-        },
-        Commands::Pause { id } => {
-            match pause_task(&mut tasks, id) {
-                Ok(_) => {
-                    save_tasks(&tasks).expect("Failed to save tasks");
-                }
-                Err(err) => match err {
-                    TaskError::NotFound => {
-                        eprintln!("Task not found");
-                    }
-                    TaskError::InvalidState(state) => {
-                        eprintln!("Invalid task state: {}", state);
-                    }
-                },
-            }
+        Commands::Start { id } => start_task(&mut tasks, id),
+        Commands::Pause { id } => pause_task(&mut tasks, id),
+        Commands::Done { id } => done_task(&mut tasks, id),
+        Commands::Delete { id } => delete_task(&mut tasks, id),
+    };
+
+    match result {
+        Ok(_) => {
             save_tasks(&tasks).expect("Failed to save tasks");
         }
-        Commands::Done { id } => match done_task(&mut tasks, id) {
-            Ok(_) => {
-                save_tasks(&tasks).expect("Failed to save tasks");
-            }
-            Err(err) => match err {
-                TaskError::NotFound => {
-                    eprintln!("Task not found");
-                }
-                TaskError::InvalidState(state) => {
-                    eprintln!("Invalid task state: {}", state);
-                }
-            },
-        },
-        Commands::Delete { id } => match delete_task(&mut tasks, id) {
-            Ok(_) => {
-                save_tasks(&tasks).expect("Failed to save tasks");
-            }
-            Err(err) => match err {
-                TaskError::NotFound => {
-                    eprintln!("Task not found");
-                }
-                TaskError::InvalidState(state) => {
-                    eprintln!("Invalid task state: {}", state);
-                }
-            },
-        },
+        Err(err) => handle_error(err),
+    }
+}
+
+fn add_task_interactive(mut tasks: &mut Vec<Task>) -> Result<(), TaskError> {
+    let name = match Text::new("Enter task name: ").prompt() {
+        Ok(name) => name,
+        Err(_) => panic!("Failed to get task name"),
+    };
+    let deadline = match DateSelect::new("Enter task deadline (YYYY-MM-DD): ")
+        .with_min_date(NaiveDate::from(Utc::now().naive_utc()))
+        .prompt()
+    {
+        Ok(deadline) => deadline,
+        Err(_) => panic!("Failed to get task deadline"),
+    };
+    let priority =
+        match Select::new("Enter task priority: ", vec!["Low", "Medium", "High"]).prompt() {
+            Ok(priority) => priority.to_string(),
+            Err(_) => panic!("Failed to get task priority"),
+        };
+    let estimated_hours: f32 = match CustomType::<f32>::new("Enter task estimated hours: ")
+        .with_formatter(&|i| format!("{:.2}h", i))
+        .with_error_message("Please type a valid number")
+        .prompt()
+    {
+        Ok(estimated_hours) => estimated_hours,
+        Err(_) => panic!("Failed to get task estimated hours"),
+    };
+
+    let status = TaskStatus::Todo;
+
+    add_task(
+        &mut tasks,
+        name,
+        status,
+        deadline,
+        priority,
+        estimated_hours,
+    )
+}
+
+fn handle_error(err: TaskError) {
+    match err {
+        TaskError::NotFound => eprintln!("Task not found"),
+        TaskError::InvalidState(state) => eprintln!("Invalid task state: {}", state),
     }
 }
